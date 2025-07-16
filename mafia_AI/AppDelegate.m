17ff0cd2962d8bdd04df4185b2888472c1906bab
gninghen123 <144544801+gninghen123@users.noreply.github.com>
2025-07-16 23:47:27 +0200

    4.00

--- mafia_AI/AppDelegate.m
+++ mafia_AI/AppDelegate.m
@@ -1,32 +1,50 @@
 //
 //  AppDelegate.m
-//  mafia_AI
+//  trading app
 //
 //  Created by fabio gattone on 16/07/25.
 //
 
 #import "AppDelegate.h"
+#import "MainWindowController.h"
+#import "DownloadManager.h"
+#import "SchwabDataSource.h"
 
 @interface AppDelegate ()
-
-@property (strong) IBOutlet NSWindow *window;
+@property (nonatomic, strong) MainWindowController *mainWindowController;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    // Registra data sources
+    [self registerDataSources];
+    
+    // Crea e mostra la finestra principale
+    self.mainWindowController = [[MainWindowController alloc] init];
+    [self.mainWindowController showWindow:self];
 }
 
+- (void)registerDataSources {
+    DownloadManager *downloadManager = [DownloadManager sharedManager];
+    
+    // Registra Schwab data source
+    SchwabDataSource *schwabSource = [[SchwabDataSource alloc] init];
+    [downloadManager registerDataSource:schwabSource
+                               withType:DataSourceTypeSchwab
+                               priority:1];
+}
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+    // Cleanup code
 }
-
 
 - (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
     return YES;
 }
 
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+    return YES;
+}
 
 @end

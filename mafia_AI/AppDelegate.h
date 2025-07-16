23ce0408aea82413611bf51a3423d9f5b6728643
gninghen123 <144544801+gninghen123@users.noreply.github.com>
2025-07-16 23:29:48 +0200

    Initial Commit

---
+++ mafia_AI/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  mafia_AI
+//
+//  Created by fabio gattone on 16/07/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ mafia_AI/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  mafia_AI
+//
+//  Created by fabio gattone on 16/07/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}

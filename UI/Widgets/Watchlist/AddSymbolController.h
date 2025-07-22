baed50a06c68d3b3c2b2386b5131848a78321d3a
gninghen123 <144544801+gninghen123@users.noreply.github.com>
2025-07-22 08:34:39 +0200

    f

---
+++ UI/Widgets/Watchlist/AddSymbolController.h
@@ -0,0 +1,16 @@
+//
+//  AddSymbolController.h
+//  mafia_AI
+//
+//  Created by fabio gattone on 22/07/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddSymbolController : NSWindowController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UI/Widgets/Watchlist/AddSymbolController.m
@@ -0,0 +1,22 @@
+//
+//  AddSymbolController.m
+//  mafia_AI
+//
+//  Created by fabio gattone on 22/07/25.
+//
+
+#import "AddSymbolController.h"
+
+@interface AddSymbolController ()
+
+@end
+
+@implementation AddSymbolController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end

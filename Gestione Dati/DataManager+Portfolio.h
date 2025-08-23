b48288877bafbab07556e63cf3b608232736ebcb
gninghen123 <144544801+gninghen123@users.noreply.github.com>
2025-08-23 14:09:51 +0200

    d

---
+++ Gestione Dati/DataManager+Portfolio.h
@@ -0,0 +1,16 @@
+//
+//  DataManager+Portfolio.h
+//  mafia_AI
+//
+//  Created by fabio gattone on 23/08/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DataManager_Portfolio : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Gestione Dati/DataManager+Portfolio.m
@@ -0,0 +1,12 @@
+//
+//  DataManager+Portfolio.m
+//  mafia_AI
+//
+//  Created by fabio gattone on 23/08/25.
+//
+
+#import "DataManager+Portfolio.h"
+
+@implementation DataManager_Portfolio
+
+@end

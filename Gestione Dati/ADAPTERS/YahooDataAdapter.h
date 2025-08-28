5310d47c84a4a6d9086651d5cb993c0e89591c36
gninghen123 <144544801+gninghen123@users.noreply.github.com>
2025-08-28 13:52:26 +0200

    d

--- Gestione Dati/ADAPTERS/YahooDataAdapter.h
+++ Gestione Dati/ADAPTERS/YahooDataAdapter.h
@@ -9,46 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "DataSourceAdapter.h"
 
-NS_ASSUME_NONNULL_BEGIN
 
 @interface YahooDataAdapter : NSObject <DataSourceAdapter>
 
-#pragma mark - DataSourceAdapter Protocol Implementation
-
-// Quote data standardization
-- (MarketData *)standardizeQuoteData:(NSDictionary *)rawData forSymbol:(NSString *)symbol;
-
-// Historical data standardization - returns HistoricalBarModel objects
-- (NSArray<HistoricalBarModel *> *)standardizeHistoricalData:(id)rawData forSymbol:(NSString *)symbol;
-
-// Order book standardization
-- (NSDictionary *)standardizeOrderBookData:(id)rawData forSymbol:(NSString *)symbol;
-
-// Batch quotes standardization
-- (NSDictionary *)standardizeBatchQuotesData:(id)rawData forSymbols:(NSArray<NSString *> *)symbols;
-
-// Account/Portfolio data (future implementation)
-- (id)standardizePositionData:(NSDictionary *)rawData;
-- (id)standardizeOrderData:(NSDictionary *)rawData;
-- (NSDictionary *)standardizeAccountData:(id)rawData;
-
-#pragma mark - Yahoo-Specific Helper Methods
-
-// Parse Yahoo Finance JSON chart response
-- (NSDictionary *)parseYahooChartResponse:(NSDictionary *)jsonResponse forSymbol:(NSString *)symbol;
-
-// Extract metadata from Yahoo response
-- (NSDictionary *)extractMetadataFromYahooResponse:(NSDictionary *)yahooResult;
-
-// Convert Yahoo timestamp arrays to NSDate objects
-- (NSArray<NSDate *> *)convertYahooTimestamps:(NSArray<NSNumber *> *)timestamps;
-
-// Validate Yahoo Finance response structure
-- (BOOL)isValidYahooResponse:(NSDictionary *)response;
-
-// Source name for debugging
-- (NSString *)sourceName;
-
 @end
-
-NS_ASSUME_NONNULL_END
